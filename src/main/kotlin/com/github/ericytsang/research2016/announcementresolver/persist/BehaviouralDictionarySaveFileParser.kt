434ecb11f158eec7c062382315839703cc270046
Eric Tsang <surplus.et@gmail.com>
2016-08-26 02:24:15 -0700

    can now save and load behaviour dictionary

---
+++ src/main/kotlin/com/github/ericytsang/research2016/announcementresolver/persist/BehaviouralDictionarySaveFileParser.kt
@@ -0,0 +1,206 @@
+package com.github.ericytsang.research2016.announcementresolver.persist
+
+import com.github.ericytsang.lib.json.Json
+import com.github.ericytsang.research2016.announcementresolver.simulation.Behaviour
+import com.github.ericytsang.research2016.propositionallogic.Proposition
+import com.github.ericytsang.research2016.propositionallogic.makeFrom
+import com.github.ericytsang.research2016.propositionallogic.toParsableString
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File
+import java.io.PrintWriter
+
+/**
+ * Created by surpl on 8/25/2016.
+ */
+object BehaviouralDictionarySaveFileParser
+{
+    /**
+     * the root node of all the json is a [JSONArray] of [JSONObject] elements
+     * that are formatted in the way described by [jsonSchema].
+     */
+    private object jsonSchema
+    {
+        /**
+         * key to [String] containing parseable [Proposition] string.
+         */
+        object proposition:Json.Schema()
+
+        /**
+         * key to [JSONObject] containing information about a [Behaviour].
+         */
+        object behaviour:Json.Schema()
+        {
+            /**
+             * key to [String] that describes which behaviour this is.
+             */
+            object name:Json.Schema()
+            {
+                enum class Value
+                {
+                    DO_NOTHING,
+                    GUARD,
+                    PATROL,
+                    WANDER
+                }
+            }
+
+            /**
+             * exclusive to [jsonSchema.proposition.behaviour.name.Value.GUARD].
+             * key to [JSONObject] describing position of agent.
+             */
+            object guard:Json.Schema()
+            {
+                /**
+                 * key to [Int] describing x position of agent.
+                 */
+                object x:Json.Schema()
+
+                /**
+                 * key to [Int] describing y position of agent.
+                 */
+                object y:Json.Schema()
+
+                /**
+                 * key to [Int] describing direction of agent.
+                 */
+                object direction:Json.Schema()
+            }
+
+            /**
+             * exclusive to [jsonSchema.proposition.behaviour.name.Value.PATROL].
+             *
+             * key to [JSONArray] containing [JSONObject] elements each of which
+             * describe a waypoint to visit while on patrol.
+             */
+            object waypoints:Json.Schema()
+            {
+                /**
+                 * key to [Int] describing x position of agent.
+                 */
+                object x:Json.Schema()
+
+                /**
+                 * key to [Int] describing y position of agent.
+                 */
+                object y:Json.Schema()
+
+                /**
+                 * key to [Int] describing direction of agent.
+                 */
+                object direction:Json.Schema()
+            }
+        }
+    }
+
+    fun load(file:File):List<BehaviourEntry>
+    {
+        return file.inputStream()
+            // read file into a string
+            .use {it.readBytes().let {String(it)}}
+            // parse the string as a JSONArray of JSONObjects
+            .let {JSONArray(it)}.map {it as JSONObject}
+            // convert JSONObjects into objects
+            .map {it.toBehaviourEntry()}
+    }
+
+    fun save(file:File,objects:List<BehaviourEntry>)
+    {
+        // convert objects into JSON
+        val json = objects
+            .map {it.toJsonObject()}
+            .let {JSONArray(it)}
+            .toString(4)
+
+        // save JSON to the file
+        file.outputStream()
+            .let {PrintWriter(it)}
+            .use {it.print(json)}
+    }
+
+    data class BehaviourEntry(
+        val proposition:Proposition,
+        val behaviour:Behaviour)
+
+    private fun BehaviourEntry.toJsonObject():JSONObject
+    {
+        return Json.obj()
+        {
+            "${jsonSchema.proposition}" mapsTo proposition.toParsableString()
+            "${jsonSchema.behaviour}" mapsTo behaviour.toJsonObject()
+        }
+    }
+
+    private fun JSONObject.toBehaviourEntry():BehaviourEntry
+    {
+        return BehaviourEntry(
+            getString("${jsonSchema.proposition}").let {Proposition.makeFrom(it)},
+            getJSONObject("${jsonSchema.behaviour}").toBehaviour())
+    }
+
+    private fun Behaviour.toJsonObject():JSONObject
+    {
+        return when(this)
+        {
+            is Behaviour.DoNothing -> Json.obj()
+            {
+                "${jsonSchema.behaviour.name}" mapsTo jsonSchema.behaviour.name.Value.DO_NOTHING.name
+            }
+            is Behaviour.Wander -> Json.obj()
+            {
+                "${jsonSchema.behaviour.name}" mapsTo jsonSchema.behaviour.name.Value.WANDER.name
+            }
+            is Behaviour.Guard -> Json.obj()
+            {
+                "${jsonSchema.behaviour.name}" mapsTo jsonSchema.behaviour.name.Value.GUARD.name
+                "${jsonSchema.behaviour.guard}" mapsTo Json.obj()
+                {
+                    "${jsonSchema.behaviour.guard.x}" mapsTo x
+                    "${jsonSchema.behaviour.guard.y}" mapsTo y
+                    "${jsonSchema.behaviour.guard.direction}" mapsTo direction.name
+                }
+            }
+            is Behaviour.Patrol -> Json.obj()
+            {
+                "${jsonSchema.behaviour.name}" mapsTo jsonSchema.behaviour.name.Value.PATROL.name
+                "${jsonSchema.behaviour.waypoints}" mapsTo waypoints.map()
+                {
+                    Json.obj()
+                    {
+                        "${jsonSchema.behaviour.guard.x}" mapsTo it.x
+                        "${jsonSchema.behaviour.guard.y}" mapsTo it.y
+                        "${jsonSchema.behaviour.guard.direction}" mapsTo it.direction.name
+                    }
+                }.let {JSONArray(it)}
+            }
+        }
+    }
+
+    private fun JSONObject.toBehaviour():Behaviour
+    {
+        return when(jsonSchema.behaviour.name.Value.valueOf(getString("${jsonSchema.behaviour.name}")))
+        {
+            jsonSchema.behaviour.name.Value.WANDER -> Behaviour.Wander()
+            jsonSchema.behaviour.name.Value.DO_NOTHING -> Behaviour.DoNothing()
+            jsonSchema.behaviour.name.Value.PATROL ->
+            {
+                val waypoints = getJSONArray("${jsonSchema.behaviour.waypoints}").map()
+                {
+                    it as JSONObject
+                    val x = it.getInt("${jsonSchema.behaviour.waypoints.x}")
+                    val y = it.getInt("${jsonSchema.behaviour.waypoints.y}")
+                    val direction = it.getString("${jsonSchema.behaviour.waypoints.direction}").let {Behaviour.CardinalDirection.valueOf(it)}
+                    Behaviour.Guard(x,y,direction)
+                }
+                Behaviour.Patrol(waypoints)
+            }
+            jsonSchema.behaviour.name.Value.GUARD ->
+            {
+                val x = getJSONObject("${jsonSchema.behaviour.guard}").getInt("${jsonSchema.behaviour.guard.x}")
+                val y = getJSONObject("${jsonSchema.behaviour.guard}").getInt("${jsonSchema.behaviour.guard.y}")
+                val direction = getJSONObject("${jsonSchema.behaviour.guard}").getString("${jsonSchema.behaviour.guard.direction}").let {Behaviour.CardinalDirection.valueOf(it)}
+                Behaviour.Guard(x,y,direction)
+            }
+        }
+    }
+}
--- src/main/kotlin/com/github/ericytsang/research2016/announcementresolver/window/BehaviouralDictionaryWindowController.kt
+++ src/main/kotlin/com/github/ericytsang/research2016/announcementresolver/window/BehaviouralDictionaryWindowController.kt
@@ -1,11 +1,16 @@
 package com.github.ericytsang.research2016.announcementresolver.window
 
+import com.github.ericytsang.lib.javafxutils.JavafxUtils
 import com.github.ericytsang.research2016.announcementresolver.guicomponent.BehavioralDictionaryTableView
+import com.github.ericytsang.research2016.announcementresolver.persist.BehaviouralDictionarySaveFileParser
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.stage.FileChooser
 import javafx.stage.Stage
+import java.io.File
+import java.nio.file.Paths
 
 /**
  * Created by surpl on 8/14/2016.
@@ -41,6 +46,30 @@ class BehaviouralDictionaryWindowController
      */
     @FXML private fun saveToFile()
     {
+        val file = FileChooser()
+            .apply()
+            {
+                title = "Save Input Data"
+                initialDirectory = File(Paths.get(".").toAbsolutePath().normalize().toString())
+                extensionFilters += FileChooser.ExtensionFilter("behaviours","*.behaviours")
+                extensionFilters += FileChooser.ExtensionFilter("json","*.json")
+                extensionFilters += FileChooser.ExtensionFilter("any","*")
+            }
+            .showSaveDialog(stage.scene.window)
+
+        if (file != null)
+        {
+            try
+            {
+                val objects = behaviouralDictionaryTableView.items
+                    .map {BehaviouralDictionarySaveFileParser.BehaviourEntry(it.proposition,it.behavior)}
+                BehaviouralDictionarySaveFileParser.save(file,objects)
+            }
+            catch (ex:Exception)
+            {
+                JavafxUtils.showErrorDialog("Save Input Data","Failed to save input data",ex)
+            }
+        }
     }
 
     /**
@@ -48,6 +77,37 @@ class BehaviouralDictionaryWindowController
      */
     @FXML private fun loadFromFile()
     {
+        // let the user pick a file to load from
+        val file = FileChooser()
+            .apply()
+            {
+                title = "Load Input Data"
+                initialDirectory = File(Paths.get(".").toAbsolutePath().normalize().toString())
+                extensionFilters += FileChooser.ExtensionFilter("behaviours","*.behaviours")
+                extensionFilters += FileChooser.ExtensionFilter("json","*.json")
+                extensionFilters += FileChooser.ExtensionFilter("any","*")
+            }
+            .showOpenDialog(stage.scene.window)
+
+        // if a file was chosen, load data from it
+        if (file != null)
+        {
+            try
+            {
+                val parsedObjects = BehaviouralDictionarySaveFileParser.load(file)
+
+                // load agents from the file
+                val objects = parsedObjects
+                    .map {BehavioralDictionaryTableView.RowData(it.proposition,it.behaviour)}
+
+                // add them to the table view
+                behaviouralDictionaryTableView.items.setAll(objects)
+            }
+            catch (ex:Exception)
+            {
+                JavafxUtils.showErrorDialog("Load Input Data","Failed to load input data",ex)
+            }
+        }
     }
 
     /**
