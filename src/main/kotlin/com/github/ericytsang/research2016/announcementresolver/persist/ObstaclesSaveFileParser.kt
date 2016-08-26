4a6ae390e673cc85b9044047a8f353a8c0b31596
Eric Tsang <surplus.et@gmail.com>
2016-08-25 23:05:41 -0700

    can now save and load obstacles from the obstacles window

---
+++ src/main/kotlin/com/github/ericytsang/research2016/announcementresolver/persist/ObstaclesSaveFileParser.kt
@@ -0,0 +1,117 @@
+package com.github.ericytsang.research2016.announcementresolver.persist
+
+import com.github.ericytsang.lib.json.Json
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File
+import java.io.PrintWriter
+
+/**
+ * Created by surpl on 8/25/2016.
+ */
+object ObstaclesSaveFileParser
+{
+    /**
+     * the root node of all the json is a [JSONArray] of [JSONObject] elements
+     * that are formatted in the way described by [jsonSchema].
+     */
+    private object jsonSchema
+    {
+
+        /**
+         * key to [JSONObject] containing coordinates of first end of wall.
+         */
+        object position1:Json.Schema()
+        {
+
+            /**
+             * key to [Int] that is x coordinate of first end of wall.
+             */
+            object x:Json.Schema()
+
+            /**
+             * key to [Int] that is y coordinate of first end of wall.
+             */
+            object y:Json.Schema()
+        }
+
+        /**
+         * key to [JSONObject] containing coordinates of second end of wall.
+         */
+        object position2:Json.Schema()
+        {
+
+            /**
+             * key to [Int] that is x coordinate of second end of wall.
+             */
+            object x:Json.Schema()
+
+            /**
+             * key to [Int] that is y coordinate of second end of wall.
+             */
+            object y:Json.Schema()
+        }
+    }
+
+    fun load(file:File):List<Obstacle>
+    {
+        return file.inputStream()
+            // read file into a string
+            .use {it.readBytes().let {String(it)}}
+            // parse the string as a JSONArray of JSONObjects
+            .let {JSONArray(it)}.map {it as JSONObject}
+            // convert JSONObjects into objects
+            .map {it.toObject()}
+    }
+
+    fun save(file:File,objects:List<Obstacle>)
+    {
+        // convert objects into JSON
+        val json = objects
+            .map {it.toJsonObject()}
+            .let {JSONArray(it)}
+            .toString(4)
+
+        // save JSON to the file
+        file.outputStream()
+            .let {PrintWriter(it)}
+            .use {it.print(json)}
+    }
+
+    data class Obstacle(
+        val position1X:Int,
+        val position1Y:Int,
+        val position2X:Int,
+        val position2Y:Int)
+
+    private fun Obstacle.toJsonObject():JSONObject
+    {
+        return Json.obj()
+        {
+            "${jsonSchema.position1}" mapsTo Json.obj()
+            {
+                "${jsonSchema.position1.x}" mapsTo position1X
+                "${jsonSchema.position1.y}" mapsTo position1Y
+            }
+            "${jsonSchema.position2}" mapsTo Json.obj()
+            {
+                "${jsonSchema.position1.x}" mapsTo position2X
+                "${jsonSchema.position1.y}" mapsTo position2Y
+            }
+        }
+    }
+
+    private fun JSONObject.toObject():Obstacle
+    {
+        val position1X = getJSONObject("${jsonSchema.position1}").getInt("${jsonSchema.position1.x}")
+        val position1Y = getJSONObject("${jsonSchema.position1}").getInt("${jsonSchema.position1.y}")
+        val position2X = getJSONObject("${jsonSchema.position2}").getInt("${jsonSchema.position2.x}")
+        val position2Y = getJSONObject("${jsonSchema.position2}").getInt("${jsonSchema.position2.y}")
+
+        return Obstacle(
+            position1X,
+            position1Y,
+            position2X,
+            position2Y)
+    }
+}
--- src/main/kotlin/com/github/ericytsang/research2016/announcementresolver/window/ObstacleWindowController.kt
+++ src/main/kotlin/com/github/ericytsang/research2016/announcementresolver/window/ObstacleWindowController.kt
@@ -1,11 +1,17 @@
 package com.github.ericytsang.research2016.announcementresolver.window
 
+import com.github.ericytsang.lib.javafxutils.JavafxUtils
+import com.github.ericytsang.lib.simulation.Simulation
 import com.github.ericytsang.research2016.announcementresolver.guicomponent.ObstacleTableView
+import com.github.ericytsang.research2016.announcementresolver.persist.ObstaclesSaveFileParser
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.stage.FileChooser
 import javafx.stage.Stage
+import java.io.File
+import java.nio.file.Paths
 
 /**
  * Created by surpl on 8/14/2016.
@@ -41,6 +47,30 @@ class ObstacleWindowController
      */
     @FXML private fun saveToFile()
     {
+        val file = FileChooser()
+            .apply()
+            {
+                title = "Save Input Data"
+                initialDirectory = File(Paths.get(".").toAbsolutePath().normalize().toString())
+                extensionFilters += FileChooser.ExtensionFilter("obstacles","*.obstacles")
+                extensionFilters += FileChooser.ExtensionFilter("json","*.json")
+                extensionFilters += FileChooser.ExtensionFilter("any","*")
+            }
+            .showSaveDialog(stage.scene.window)
+
+        if (file != null)
+        {
+            try
+            {
+                val objects = obstacleTableView.items
+                    .map {ObstaclesSaveFileParser.Obstacle(it.cell1.x,it.cell1.y,it.cell2.x,it.cell2.y)}
+                ObstaclesSaveFileParser.save(file,objects)
+            }
+            catch (ex:Exception)
+            {
+                JavafxUtils.showErrorDialog("Save Input Data","Failed to save input data",ex)
+            }
+        }
     }
 
     /**
@@ -48,6 +78,39 @@ class ObstacleWindowController
      */
     @FXML private fun loadFromFile()
     {
+        // let the user pick a file to load from
+        val file = FileChooser()
+            .apply()
+            {
+                title = "Load Input Data"
+                initialDirectory = File(Paths.get(".").toAbsolutePath().normalize().toString())
+                extensionFilters += FileChooser.ExtensionFilter("obstacles","*.obstacles")
+                extensionFilters += FileChooser.ExtensionFilter("json","*.json")
+                extensionFilters += FileChooser.ExtensionFilter("any","*")
+            }
+            .showOpenDialog(stage.scene.window)
+
+        // if a file was chosen, load data from it
+        if (file != null)
+        {
+            try
+            {
+                val parsedObjects = ObstaclesSaveFileParser.load(file)
+
+                // load agents from the file
+                val objects = parsedObjects
+                    .map {ObstacleTableView.RowData(
+                        Simulation.Cell.getElseMake(it.position1X,it.position1Y),
+                        Simulation.Cell.getElseMake(it.position2X,it.position2Y))}
+
+                // add them to the table view
+                obstacleTableView.items.setAll(objects)
+            }
+            catch (ex:Exception)
+            {
+                JavafxUtils.showErrorDialog("Load Input Data","Failed to load input data",ex)
+            }
+        }
     }
 
     /**
